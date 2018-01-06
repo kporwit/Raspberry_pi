ae1f0e8b8b092558853f321b03c15053ec214a99
Kamil Porwit (Pi) <kamilporwit93@gmail.com>
2018-01-06 17:15:34 +0100

    added comentaries how to use files

--- cherrymusic/cherrymusicupstart.sh
+++ cherrymusic/cherrymusicupstart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#put in: usr/bin/local/
+
 echo "Starting Cherrymusic server at port 7600"
 
 screen -d -m python /opt/cherrymusic/cherrymusic
 
+#make executable: chmod 744 /esr/local/bin/cherrymusicupstart.sh
